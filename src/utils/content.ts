9883629e3753d33080f2fc2b63e32f72e93f9241
KleoPetroff <kleopetroff@gmail.com>
2021-06-12 15:22:29 +0300

    refactor(content): return null if no book is found

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -1,20 +1,24 @@
 import isEmpty from 'lodash-es/isEmpty'
 import { Book, BookMeta, Books, Response } from '../types'
 
-const getFromSingleRecord = (book: Book, title: string): BookMeta => {
-  if (book.title === title) {
-    return {
-      id: book.id,
-      slug: book.slug
-    }
+type BookMetaReturnType = BookMeta | null
+
+const getFromSingleRecord = (book: Book, title: string): BookMetaReturnType => {
+  if (book.title !== title) {
+    return null
+  }
+
+  return {
+    id: book.id,
+    slug: book.slug
   }
 }
 
-const getFromMultipleRecords = (books: Book[], title: string): BookMeta => {
+const getFromMultipleRecords = (books: Book[], title: string): BookMetaReturnType => {
   const book = books.find(text => text.title === title)
 
   if (isEmpty(book)) {
-    return
+    return null
   }
 
   return {
@@ -23,7 +27,7 @@ const getFromMultipleRecords = (books: Book[], title: string): BookMeta => {
   }
 };
 
-export function getData(data: Response<Books>, title: string) {
+export function getData(data: Response<Books>, title: string): BookMetaReturnType {
   const books = data.results.books.book
 
   if (!Array.isArray(books)) {

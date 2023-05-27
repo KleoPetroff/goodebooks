86ef413158049ebedc6f0798de23bb91713d6a31
KleoPetroff <kleopetroff@gmail.com>
2023-05-27 13:36:58 +0300

    refactor: mark unused variable, change log to error

--- src/background.ts
+++ src/background.ts
@@ -2,12 +2,16 @@ import { Chitanka } from './integrations/chitanka'
 import { ChromeContentRequest, Integrations } from './shared/types'
 
 chrome.runtime.onMessage.addListener(
-  (request: ChromeContentRequest, sender, sendResponse) => {
+  (
+    request: ChromeContentRequest,
+    _sender: chrome.runtime.MessageSender,
+    sendResponse: (response?: any) => void
+  ) => {
     if (request.type === Integrations.CHITANKA) {
       Chitanka.getInstance()
         .fetch(request)
         .then((book) => sendResponse(book))
-        .catch((error) => console.log(error))
+        .catch((error) => console.error(error.message))
     }
 
     return true // Will respond asynchronously.

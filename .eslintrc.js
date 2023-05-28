db6cc3926c32a52d46ec9331cbb2142616a1bf6b
KleoPetroff <kleopetroff@gmail.com>
2023-05-28 21:34:35 +0300

    chore: ignore node_modules, build files and configuration files from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,13 +13,25 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint', 'prettier'],
-  rules: {
-    indent: ['error', 2],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
-  }
+  ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.js', 'tsconfig.json'],
+  overrides: [
+    {
+      files: ['src/**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended'
+      ],
+      plugins: ['@typescript-eslint', 'prettier'],
+      rules: {
+        indent: ['error', 2],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off'
+      }
+    }
+  ]
 }
